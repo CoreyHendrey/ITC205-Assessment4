937d703129781950bd2eaacf9454588d303a93ff
coreyhendrey <coreyhendrey@gmail.com>
2018-10-05 07:15:45 +1000

    Added scenario test for Bug2
    Added the test to check if the room can be charged a service after it is
    already checked out

---
+++ test/hotel/service/RecordServiceCTLScenario.java
@@ -0,0 +1,62 @@
+package hotel.service;
+import static org.mockito.Mockito.verify;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import hotel.booking.BookingCTL;
+import hotel.checkin.CheckinCTL;
+import hotel.checkout.CheckoutCTL;
+import hotel.credit.CreditCardType;
+import hotel.entities.Hotel;
+import hotel.entities.RoomType;
+
+@ExtendWith(MockitoExtension.class)
+class RecordServiceCTLScenario
+{
+	@Spy Hotel hotel = new Hotel();
+	@Mock RecordServiceUI recordServiceUI;
+	BookingCTL bookingCtl = new BookingCTL(hotel);
+	CheckinCTL checkinCtl = new CheckinCTL(hotel);
+	CheckoutCTL checkoutCtl = new CheckoutCTL(hotel);
+
+
+	@InjectMocks RecordServiceCTL recordCtrl = new RecordServiceCTL(hotel);
+
+	@BeforeEach
+	void setup() throws Exception
+	{
+		hotel.addRoom(RoomType.SINGLE, 102);
+	}
+
+	@Test
+	void test()
+	{
+		bookingCtl.phoneNumberEntered(1);
+		bookingCtl.guestDetailsEntered("Test", "Address");
+		bookingCtl.roomTypeAndOccupantsEntered(RoomType.SINGLE, 1);
+
+		Date d = new GregorianCalendar(1111, 0, 1).getTime();
+		bookingCtl.bookingTimesEntered(d, 1);
+		bookingCtl.creditDetailsEntered(CreditCardType.MASTERCARD, 1, 1);
+
+		checkinCtl.confirmationNumberEntered(101111102);
+		checkinCtl.checkInConfirmed(true);
+
+		checkoutCtl.roomIdEntered(102);
+		checkoutCtl.chargesAccepted(true);
+		checkoutCtl.creditDetailsEntered(CreditCardType.MASTERCARD, 1, 1);
+
+		recordCtrl.roomNumberEntered(102);
+
+		verify(recordServiceUI).displayMessage("No active booking for room id: 102");
+	}
+}
